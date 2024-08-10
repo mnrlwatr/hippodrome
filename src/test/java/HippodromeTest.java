7d182cf6acd945d3d62db237c57ccd00f4c672f6
Adm Syd <adm_syd@icloud.com>
2024-08-10 15:37:44 +0500

    a. Конструктор    Проверить, что при передаче в конструктор пустого списка, будет выброшено IllegalArgumentException;    Проверить, что при передаче в конструктор пустого списка, выброшенное исключение будет содержать сообщение "Horses cannot be empty.";

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -10,5 +12,11 @@ class HippodromeTest {
     void nullHorsesListException() {
         Exception exception=assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
         assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+
+    @Test
+    void emptyHorsesListException(){
+        Exception exception=assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+        assertEquals( "Horses cannot be empty.", exception.getMessage());
     }
 }

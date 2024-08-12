ec449ae3f60305924a0be9d2f892348ef11b403d
Adm Syd <adm_syd@icloud.com>
2024-08-12 16:04:32 +0500

    b. метод getHorses   - Проверить, что метод возвращает список, который содержит те же объекты и в той же последовательности, что и список который был передан в конструктор. При создании объекта Hippodrome передай в конструктор список из 30 разных лошадей;

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -2,9 +2,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
 
@@ -18,5 +19,17 @@ class HippodromeTest {
     void emptyHorsesListException(){
         Exception exception=assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
         assertEquals( "Horses cannot be empty.", exception.getMessage());
+    }
+
+    @Test
+    void getHorses(){
+        List<Horse> horses = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 1; i <31; i++) {
+            horses.add(new Horse("H"+i, random.nextInt(10,51), random.nextInt(100,501)));
+        }
+        Hippodrome hippodrome = new Hippodrome(horses);
+        List<Horse> listFromGetHorse = hippodrome.getHorses();
+        assertIterableEquals(horses,listFromGetHorse);
     }
 }
